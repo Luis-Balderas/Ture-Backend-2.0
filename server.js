3e2bcfd86a8c85530edd07492d56232203ddb92b
Luis Moises Balderas Garcias <bluisqr@gmail.com>
2020-08-11 10:25:27 -0500

    Trabajo implementado por Carol db, y server

--- server.js
+++ server.js
@@ -3,20 +3,28 @@ var app = express();
 const server = require('http').Server(app);
 
 
+require('dotenv').config();
+require('./configure/db');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
 const bodyParser = require('body-parser');
 const socket = require('./socket');
 const router = require('./network/routes');
-const db = require('./db');
-db('mongodb+srv://Platzi:Soyunpan123@cluster0.3dagj.mongodb.net/Ture?retryWrites=true&w=majority');
 
 socket.conncet(server);
 
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser());
 
 router(app);
 
 app.use('/app', express.static('public'));
+
+app.use(cors);
 
 app.listen(3000, function() {
     console.log('La aplicacion esta escuchando en http://localhost:3000');
